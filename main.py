4ef7cc05442f48a4d29a4faa7fb94c6f9ec7c384
durovo <aryadhruv@gmail.com>
2019-07-24 22:22:37 +0530

    Add buttons for excercises

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request,jsonify
 from flask_socketio import SocketIO,emit
-from GymTrainerBot import *
+# from GymTrainerBot import *
 import demo
 import threading
 # import mayank
@@ -34,32 +34,32 @@ def handle_connection():
 def home():
     return render_template("template.html")
 
-@app.route("/bot")
-def startTrainerBot():
-    startBot()
-    return "nothing just run bot"
+# @app.route("/bot")
+# def startTrainerBot():
+#     startBot()
+#     return "nothing just run bot"
 
-@app.route("/botintro")
-def startTrainerBotIntro():
-    greet = startBotGreeting()
-    return jsonify(buttonName = "Your Name?",botanswers=greet)
+# @app.route("/botintro")
+# def startTrainerBotIntro():
+#     greet = startBotGreeting()
+#     return jsonify(buttonName = "Your Name?",botanswers=greet)
 
-@app.route("/humanIntro")
-def startTrainerHumanIntro():
-    greet = humanIntroduction()
-    return jsonify(buttonName = "Hey there!",botanswers=greet)
+# @app.route("/humanIntro")
+# def startTrainerHumanIntro():
+#     greet = humanIntroduction()
+#     return jsonify(buttonName = "Hey there!",botanswers=greet)
 
-@app.route("/askExercise")
-def startTrainerAskExercise():
-    greet = askExercise()
-    if "Great" in greet:
-        demo.start_planks(0)
-    return jsonify(buttonName = "Lets do it!",botanswers=greet)
+# @app.route("/askExercise")
+# def startTrainerAskExercise():
+#     greet = askExercise()
+#     if "Great" in greet:
+#         demo.start_planks(0)
+#     return jsonify(buttonName = "Lets do it!",botanswers=greet)
 
-@app.route("/trainer")
-def startTrainerForced():
-    demo.start_planks(1)
-    return "nothing"
+# @app.route("/trainer")
+# def startTrainerForced():
+#     demo.start_planks(1)
+#     return "nothing"
 
 if __name__ == "__main__":
     # app.run(debug=True) 
